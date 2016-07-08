4d91815e841ae1254bcd575f3129fd9ce7641d9c
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-07-08 15:30:15 +0545

    Use fontawesome close icon glyph for popup

--- includes/views/html-view-tmpl-modal-sharing.php
+++ includes/views/html-view-tmpl-modal-sharing.php
@@ -13,7 +13,7 @@ $social_networks = ess_get_core_supported_social_networks();
 <div class="ess-popup-overlay"></div>
 <div class="ess-pinterest-popup-overlay"></div>
 <div class="ess-popup-wrapper">
-	<div class="ess-popup-close"> X </div>
+	<div class="ess-popup-close"><i class="fa fa-close"></i></div>
 	<div id="ess-wrap-inline-networks">
 		<ul class="ess-social-network-lists">
 			<?php foreach ( $social_networks as $network_id => $network ) :
